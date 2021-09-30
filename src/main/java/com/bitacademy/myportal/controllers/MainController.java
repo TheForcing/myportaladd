744701be5d9d0c2f792ffd1db7738eaf34366efa
박지훈 <qkrw6@naver.com>
2021-09-30 22:34:17 +0900

    사용자 서비스

---
+++ src/main/java/com/bitacademy/myportal/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.bitacademy.myportal.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	@RequestMapping({"/", "/main"})
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView();
+//		mav.setViewName("/WEB-INF/views/home.jsp");
+		mav.setViewName("home");	//	ViewResolver가 prefix와 suffix 조함 -> 완전한 ViewName 리턴
+		return mav;
+	}
+}
